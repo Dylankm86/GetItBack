38c7dd51ccda504f97f5a96274ae458240c8840e
Dylankm86 <335478@student.mobutrecht.nl>
2024-03-25 13:14:51 +0100

    Refactor authentication logic and navigation links

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
-    //
+    public function index()
+    {
+        if (Auth::id()) {
+            $usertype = auth()->user()->usertype;
+            if ($usertype == 'user') {
+                return view('dashboard');
+            } else if ($usertype == 'admin') {
+                return view('admin.adminhome');
+            } else {
+                return redirect('/login');
+            }
+        }
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,6 +24,7 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index']);
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
