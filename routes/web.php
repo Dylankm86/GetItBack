d1cac6cfa4005ad15df5d6de155a43d2d8c56640
Dylankm86 <335478@student.mobutrecht.nl>
2024-03-25 20:33:13 +0100

    Remove unnecessary code and update routes

--- routes/web.php
+++ routes/web.php
@@ -25,8 +25,6 @@ Route::get('/', function () {
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->middleware('auth')->name('home');
 
-Route::get('post', [HomeController::class, 'index'])->name('post')->middleware('auth', 'admin');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
